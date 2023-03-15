5922d21c139bc457fcbfcda7d00a0ccc9df68404
mjosh51 <mayorjoshua51@gmail.com>
2023-03-15 04:10:11 -0500

    complete

---
+++ v1/routes/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { Verify, VerifyRole } from '../middleware/verify.js';
+import Auth from './auth.js';
+
+const app = express();
+
+app.disable('x-powered-by'); //Reduce fingerprinting
+app.get('/favico.ico', (req, res) => {
+  res.sendStatus(404);
+});
+app.get('/v1', (req, res) => {
+  try {
+    res.set('Content-Security-Policy', "default-src 'self'");
+    res.status(200).json({
+      status: 'success',
+      data: [],
+      message: 'Welcome to our API homepage!',
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+});
+app.use('/v1/auth', Auth);
+app.get('/v1/admin', Verify, VerifyRole, (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to the Admin portal!',
+  });
+});
+export default app;
